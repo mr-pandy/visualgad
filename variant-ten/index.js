3e15d54d56b4174ebc09573ac5bd8bf975977946
mr-pandy <ekerekepreciousimeh@gmail.com>
2020-07-11 04:53:50 +0100

    Made changes to the Slider

--- variant-ten/index.js
+++ variant-ten/index.js
@@ -1,5 +1,6 @@
+const mobNav = document.getElementById('mobNav');
 const navToggle = document.querySelector('.navbar-toggler');
-const navLinks = document.querySelector('menu-wrap');
+const navLinks = document.querySelector('.collapse');
 const header =  document.querySelector('header');
 const spanContent = document.querySelector('.navbar-toggler-icon');
 
@@ -13,7 +14,22 @@ navToggle.addEventListener('click', ()=>{
     //     navLinks.classList.add('menu-wrap');
     // }
 //    spanContent.innerHTML = `<img src="/variant-ten/assets/src/close.png" alt="">`
-    // header.classList.add('mob')
-    // navLinks.classList.toggle('menu-wrap')
+    //navLinks.classList.toggle('menu-wrap')
     // header.classList.remove('mob')
 });
+
+
+// Slick Slider Configuration
+$(document).ready(function () {
+    // Homepage Sliders
+    $('.home-sliders').slick({
+      autoplaySpeed: 2000,
+      slidesToShow: 3,
+      dots: true,
+      prevArrow: $('.prev-btn'),
+      nextArrow: $('.next-btn'),
+    })
+  
+   
+  })
+
